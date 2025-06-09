afa360f1b93d43bce213954e5d8ce72937387903
Ayrton Bryant Corniel García <Bryantcorniel02@gmail.com>
2025-06-08 22:28:04 -0400

    Haciendo los módulos de ventas, hoy se ha ha hecho el GetVentaById, GetVentas y CreateVentas, es posible que haya que hacer cambios en un futuro

---
+++ backend/routes/ventasController.route.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import {
+  getVentas,
+  getVentaById,
+  createVenta,
+} from '../controllers/ventasController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+
+const router = Router();
+
+router.use(authMiddleware); // proteger todas las rutas de ventas
+
+router.get('/', getVentas);
+router.get('/:id', getVentaById);
+router.post('/', createVenta); // sirve tanto para manual como directa
+
+export default router;
