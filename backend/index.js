e35349a165654fc0e08d20634f64fae39836c602
Ayrton Bryant Corniel García <Bryantcorniel02@gmail.com>
2025-07-09 04:14:53 -0400

    Ya aprendí creo 3

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173', // usa http para desarrollo local
+    origin: ['http://localhost:5173', 'https://localhost:5173'],
     credentials: true,
   })
 );
