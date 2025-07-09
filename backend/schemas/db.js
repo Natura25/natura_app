53d7c3f454208dcee8d5bae57a0dab4779cf898e
Ayrton Bryant Corniel García <Bryantcorniel02@gmail.com>
2025-07-09 01:27:45 -0400

    Cambio hecho a la conexión a la base de datos para que sea más rapida y organizada para el deploy

--- backend/schemas/db.js
+++ backend/schemas/db.js
@@ -7,18 +7,20 @@ const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT), // mejor asegurar que sea número
   ssl: { rejectUnauthorized: false }, // importante para Supabase
 });
 
-//! Revisa si la base de datos está conectada (POSTGRESQL)
-
-try {
-  const result = await db.query('SELECT NOW()');
-  console.log('🟢 Conectado a la base de datos:', result.rows[0].now);
-} catch (error) {
-  console.error('🔴 Error al conectar a la base de datos', error);
-  process.exit(1); // Sale del proceso si no se puede conectar a la base de datos
+async function testConnection() {
+  try {
+    const result = await db.query('SELECT NOW()');
+    console.log('🟢 Conectado a la base de datos:', result.rows[0].now);
+  } catch (error) {
+    console.error('🔴 Error al conectar a la base de datos', error);
+    process.exit(1); // Sale del proceso si no se puede conectar a la base de datos
+  }
 }
+
+testConnection();
 
 export default db;
